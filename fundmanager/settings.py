dd0128f6abf44cf747b6f0434fdbe28a50ea6ca4
moroka247 <morokam@gmail.com>
2025-01-25 07:50:39 +0200

    reconfig static

--- fundmanager/settings.py
+++ fundmanager/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'NAME': 'fundmanager',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'Nn522H'
+        'PASSWORD': 'root'
 
     }
 }
@@ -143,13 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STAICFILES_DIRS = [
-    os.path.join(BASE_DIR,'fundmanager/static')
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = 'fundmanager/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [BASE_DIR / "media"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
